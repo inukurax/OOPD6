df2cdf7a0bc363389b5fb85bfb2396ba2aaeacf4
Hjalte <Hjalte@Hjalte-Home-PC>
2013-01-13 17:43:45 +0100

    conflict

--- src/gui/SpreadsheetSelectionListener.java
+++ src/gui/SpreadsheetSelectionListener.java
@@ -4,7 +4,6 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
 
 import spreadsheet.Application;
-import spreadsheet.CycleException;
 import spreadsheet.Expression;
 import spreadsheet.Position;
 
